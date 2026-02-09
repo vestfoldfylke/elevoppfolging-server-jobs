b67e3014b1951f9181f93194e611900d73a5845f
Jørgen Thorsnes <jorgen.thorsnes@gmail.com>
2026-02-09 18:17:49 +0100

    fix: dont use type casting unless I have to... also, no stupid ; from biome

--- src/lib/db/mock-db-client.ts
+++ src/lib/db/mock-db-client.ts
@@ -31,13 +31,17 @@ export class MockDbClient implements IDbClient {
 	}
 
 	async replaceStudents(students: (DbAppStudent | NewAppStudent)[]): Promise<void> {
-		students.forEach((student) => {
-			if (!("_id" in student)) {
-				;(student as DbAppStudent)._id = new ObjectId()
+		const withIds: DbAppStudent[] = students.map((student) => {
+			if ("_id" in student) {
+				return student
+			}
+			return {
+				...student,
+				_id: new ObjectId()
 			}
 		})
-		mockDb.students = students as DbAppStudent[]
-		writeFileSync(`${this.debugFolderPath}/mock-students.json`, JSON.stringify(students, null, 2))
+		mockDb.students = withIds
+		writeFileSync(`${this.debugFolderPath}/mock-students.json`, JSON.stringify(withIds, null, 2))
 	}
 
 	async getUsers(): Promise<DbAppUser[]> {
@@ -45,13 +49,17 @@ export class MockDbClient implements IDbClient {
 	}
 
 	async replaceUsers(users: (DbAppUser | NewAppUser)[]): Promise<void> {
-		users.forEach((user) => {
-			if (!("_id" in user)) {
-				;(user as DbAppUser)._id = new ObjectId()
+		const withIds: DbAppUser[] = users.map((user) => {
+			if ("_id" in user) {
+				return user
+			}
+			return {
+				...user,
+				_id: new ObjectId()
 			}
 		})
-		mockDb.users = users as DbAppUser[]
-		writeFileSync(`${this.debugFolderPath}/mock-users.json`, JSON.stringify(users, null, 2))
+		mockDb.users = withIds
+		writeFileSync(`${this.debugFolderPath}/mock-users.json`, JSON.stringify(withIds, null, 2))
 	}
 
 	async getAccess(): Promise<DbAccess[]> {
@@ -59,12 +67,16 @@ export class MockDbClient implements IDbClient {
 	}
 
 	async replaceAccess(accesses: (DbAccess | NewAccess)[]): Promise<void> {
-		accesses.forEach((access) => {
-			if (!("_id" in access)) {
-				;(access as DbAccess)._id = new ObjectId()
+		const withIds: DbAccess[] = accesses.map((access) => {
+			if ("_id" in access) {
+				return access
+			}
+			return {
+				...access,
+				_id: new ObjectId()
 			}
 		})
-		mockDb.access = accesses as DbAccess[]
-		writeFileSync(`${this.debugFolderPath}/mock-access.json`, JSON.stringify(accesses, null, 2))
+		mockDb.access = withIds
+		writeFileSync(`${this.debugFolderPath}/mock-access.json`, JSON.stringify(withIds, null, 2))
 	}
 }
